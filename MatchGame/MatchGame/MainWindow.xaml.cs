541b57edff82e6c55a23b2fa15e6b3a90022fd9f
codingxllama <osamaalchalabi2@gmail.com>
2022-02-13 15:07:15 -0500

    Added user interactivity on mouse click

--- MatchGame/MatchGame/MainWindow.xaml.cs
+++ MatchGame/MatchGame/MainWindow.xaml.cs
@@ -52,5 +52,44 @@ namespace MatchGame
 
 
         }
+
+
+
+        TextBlock lastTextBlockClicked;
+
+        //Keeping track if a player has clicked on the first animal in a pair and is now trying to find its match.
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            TextBlock textBlock = sender as TextBlock;
+
+            // If a player clicked the first item in a pair, we make that item invisiable and store its textBlock in case if we need to find a match or not.
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+
+            }
+            // This indicates that we have found a match and we can proceed to find more matching pairs
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+
+            else
+            {
+                // This indicates that if the last selected item does not match the first, we make that 2nd item visisble and continue to search for a match.
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+
+            }
+
+
+
+        }
     }
 }
